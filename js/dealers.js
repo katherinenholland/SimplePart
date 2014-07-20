a04debfa7af74ac867e58b472ff6d60ad2e50d8d
katherinenholland <katherinenholland@gmail.com>
2014-07-20 17:32:03 -0400

    First upload of project files

---
+++ js/dealers.js
@@ -0,0 +1,93 @@
+var data = { "dealers":[
+				{
+					"Name": "Ciocca Subaru Parts",
+					"Lat": 40.569669,
+					"Lng": -75.548910,
+					"Map": "map1",
+					"Title": "1. Ciocca Subaru Parts",
+					"Address1": "4611 Hamilton Blvd",
+					"Address2": "Allentown, PA, 18103",
+					"Country":"USA",
+					"Phone": "855.370.3424",
+					"Review": "Our almost brand new Subaru was totaled in the hail storm in Reading. Since we were happy with the sales experience from our last purchase, we were leery about stepping out into the car buying arena so soon. However, our Ciocca experience was a very surprising experience. Our salesman Mike Vannote was very knowledgeable about the cars, very personable, and put no pressure on us. He worked with us to get the car we wanted and at a price which we thought was very good, without all the haggling. When we picked up the car is was a very relaxed, professional atmosphere and the car looked great and met all our expectations. We are very happy with our purchase and feel very good about the entire experience.",
+					"ReviewCount": "62"
+
+				},
+			
+
+				{
+					"Name":"Donaldsons Subaru Parts",
+					"Lat": 40.767104,
+					"Lng": -73.069267,
+					"Map": "map2",
+					"Title": "2. Donaldsons Subaru Parts",
+					"Address1": "5700 Sunrise Hwy",
+					"Address2": "Sayville, NY, 11782",
+					"Country":"USA",
+					"Phone": "631.567.8100",
+					"Review": "John did an excellent job assessing my needs and recommending the right vehicle. I felt he was being honest with me from the onset which made the experience easier. I have been in sales for over 30 years and dont like to waist time going back and forth on price etc. as you usually do in most dealerships. I had an appointment for 1pm and drove the vehicle home by 4:00 the same day. I was surprised his manager didnt come in and thank me for my business. I had asked John to throw in a set of splash guards on the car and his boss flat out refused. I guess spending $22K for the car couldnt possibly warrant a set of splash guards vauled at $147.00. He should have come in and explained his reason behind his decision rather than leaving it to John to explain.",
+					"ReviewCount" : "152"
+				},
+			
+
+
+				{
+					"Name":"Glanzmann",
+					"Lat": 40.091406,
+					"Lng": -75.126497,
+					"Map": "map3",
+					"Title": "2. Donaldsons Subaru Parts",
+					"Address1": "5700 Sunrise Hwy",
+					"Address2": "Sayville, NY, 11782",
+					"Country":"USA",
+					"Phone": "631.567.8100",
+					"Review": "John did an excellent job assessing my needs and recommending the right vehicle. I felt he was being honest with me from the onset which made the experience easier. I have been in sales for over 30 years and dont like to waist time going back and forth on price etc. as you usually do in most dealerships. I had an appointment for 1pm and drove the vehicle home by 4:00 the same day. I was surprised his manager didnt come in and thank me for my business. I had asked John to throw in a set of splash guards on the car and his boss flat out refused. I guess spending $22K for the car couldnt possibly warrant a set of splash guards vauled at $147.00. He should have come in and explained his reason behind his decision rather than leaving it to John to explain.",
+					"ReviewCount" : "152"
+				},
+			
+
+				{
+					"Name": "Kearny",
+					"Lat": 32.828998,
+					"Lng": -117.152930,
+					"Map": "map4",
+					"Title": "2. Donaldsons Subaru Parts",
+					"Address1": "5700 Sunrise Hwy",
+					"Address2": "Sayville, NY, 11782",
+					"Country":"USA",
+					"Phone": "631.567.8100",
+					"Review": "John did an excellent job assessing my needs and recommending the right vehicle. I felt he was being honest with me from the onset which made the experience easier. I have been in sales for over 30 years and dont like to waist time going back and forth on price etc. as you usually do in most dealerships. I had an appointment for 1pm and drove the vehicle home by 4:00 the same day. I was surprised his manager didnt come in and thank me for my business. I had asked John to throw in a set of splash guards on the car and his boss flat out refused. I guess spending $22K for the car couldnt possibly warrant a set of splash guards vauled at $147.00. He should have come in and explained his reason behind his decision rather than leaving it to John to explain.",
+					"ReviewCount" : "152"
+				},
+		
+
+				{
+					"Name": "Rafferty",
+					"Lat": 39.976402,
+					"Lng": -75.434940,
+					"Map": "map5",
+					"Title": "2. Donaldsons Subaru Parts",
+					"Address1": "5700 Sunrise Hwy",
+					"Address2": "Sayville, NY, 11782",
+					"Country":"USA",
+					"Phone": "631.567.8100",
+					"Review": "John did an excellent job assessing my needs and recommending the right vehicle. I felt he was being honest with me from the onset which made the experience easier. I have been in sales for over 30 years and dont like to waist time going back and forth on price etc. as you usually do in most dealerships. I had an appointment for 1pm and drove the vehicle home by 4:00 the same day. I was surprised his manager didnt come in and thank me for my business. I had asked John to throw in a set of splash guards on the car and his boss flat out refused. I guess spending $22K for the car couldnt possibly warrant a set of splash guards vauled at $147.00. He should have come in and explained his reason behind his decision rather than leaving it to John to explain.",
+					"ReviewCount" : "152"
+				},
+			
+
+				{
+					"Name": "Autobarn",
+					"Lat": 41.778537,
+					"Lng": -87.870739,
+					"Map": "map6",
+					"Title": "2. Donaldsons Subaru Parts",
+					"Address1": "5700 Sunrise Hwy",
+					"Address2": "Sayville, NY, 11782",
+					"Country":"USA",
+					"Phone": "631.567.8100",
+					"Review": "John did an excellent job assessing my needs and recommending the right vehicle. I felt he was being honest with me from the onset which made the experience easier. I have been in sales for over 30 years and dont like to waist time going back and forth on price etc. as you usually do in most dealerships. I had an appointment for 1pm and drove the vehicle home by 4:00 the same day. I was surprised his manager didnt come in and thank me for my business. I had asked John to throw in a set of splash guards on the car and his boss flat out refused. I guess spending $22K for the car couldnt possibly warrant a set of splash guards vauled at $147.00. He should have come in and explained his reason behind his decision rather than leaving it to John to explain.",
+					"ReviewCount" : "152"
+				}
+			
+]};
